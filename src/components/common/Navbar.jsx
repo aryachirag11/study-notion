aca99451a0a4c3196320102868149df58ca067d9
aryachirag11 <aryachirag11@gmail.com>
2024-07-19 15:11:25 +0530

    add navbar to app

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,103 +1,138 @@
-import React, { useState } from "react";
-import Logo from "../../assets/Logo/Logo-Full-Light.png";
-import { Link, useLocation } from "react-router-dom";
+import React, { useEffect } from "react";
+import logo from "../../assets/Logo/Logo-Full-Light.png";
+import { Link, matchPath } from "react-router-dom";
 import { NavbarLinks } from "../../data/navbar-links";
-import { FaAngleDown, FaSearch, FaUserAstronaut } from "react-icons/fa";
-// import SecondaryButton from "./SecondaryButton";
-import { useSelector, useDispatch } from "react-redux";
-import { loginUser, logoutUser } from "../../redux/slices/authSlice";
+import { useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
 import { AiOutlineShoppingCart } from "react-icons/ai";
-import { FiSearch, FiShoppingCart, FiUser } from "react-icons/fi";
+import ProfileDropdown from "../core/Auth/ProfiledropDown";
+import { apiConnector } from "../../services/apiConnector";
+import { categories } from "../../services/apiUrls";
+import { useState } from "react";
+import { IoIosArrowDropdownCircle } from "react-icons/io";
+
+const subLinks = [
+  {
+    title: "python",
+    link: "/catalog/python",
+  },
+  {
+    title: "web dev",
+    link: "/catalog/web-development",
+  },
+];
+
 const Navbar = () => {
-  const [currNavPath, setCurrNavPath] = useState("Home");
-  const location = useLocation();
-  const { isLoggedIn, token } = useSelector((state) => state.auth);
+  console.log("Printing base url: ", process.env.REACT_APP_BASE_URL);
+  const { token } = useSelector((state) => state.auth);
   const { user } = useSelector((state) => state.profile);
-  const { totalItems, cartItems } = useSelector((state) => state.cart);
-  const dispatch = useDispatch();
-  const handleClick = () => {
-    console.log("Login / Sign in clicked");
+  const { totalItems } = useSelector((state) => state.cart);
+  const location = useLocation();
+
+  const [ssubLinks, setSsubLinks] = useState([]);
+
+  const fetchSublinks = async () => {
+    try {
+      const result = await apiConnector("GET", categories.CATEGORIES_API);
+      console.log("Printing Sublinks result:", result);
+      setSsubLinks(result.data.data);
+    } catch (error) {
+      console.log("Could not fetch the category list");
+    }
   };
-  // const matchRoute = (route) => {
-  //   return matchRoute({ path: route }, location.pathname);
-  // };
+
+  useEffect(() => {
+    fetchSublinks();
+  }, []);
+
+  const matchRoute = (route) => {
+    return matchPath({ path: route }, location.pathname);
+  };
 
   return (
-    <div className="w-full h-14 border-b  border-b-richblack-700 py-3 px-28">
-      <div className="w-11/12 max-w-maxContent justify-center items-center flex gap-8">
-        <Link className="w-40 h-8" to={"/"}>
-          <img src={Logo} alt="Logo" />
+    <div className="flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700">
+      <div className="flex w-11/12 max-w-maxContent items-center justify-between">
+        {/* Image */}
+        <Link to="/">
+          <img src={logo} width={160} height={42} loading="lazy" />
         </Link>
-        <nav className=" grow h-8 flex gap-6 justify-center items-center text-base text-richblack-25 font-normal font-inter">
-          {NavbarLinks.map((item, index) => (
-            <li
-              key={index}
-              className="list-none hover:text-yellow-50 hover:scale-95 transition-all duration-200"
-            >
-              {item.title === "Catalog" ? (
-                <div className="flex gap-1 items-center cursor-pointer">
-                  <p
-                    className={` ${
-                      currNavPath === item?.title ? "text-yellow-50" : ""
-                    }
-              `}
-                  >
-                    {item.title}
-                  </p>
-                  <FaAngleDown className="w-4 h-4" />
-                </div>
-              ) : (
-                <Link
-                  to={item?.path}
-                  onClick={(e) => setCurrNavPath(e.target.innerText)}
-                  className={` ${
-                    currNavPath === item?.title ? "text-yellow-50" : ""
-                  }
-              `}
-                >
-                  {item.title}
-                </Link>
-              )}
-            </li>
-          ))}
+
+        {/* Nav Links */}
+        <nav>
+          <ul className="flex gap-x-6 text-richblack-25">
+            {NavbarLinks.map((link, index) => (
+              <li key={index}>
+                {link.title === "Catalog" ? (
+                  <div className="relative flex items-center gap-2 group">
+                    <p>{link.title}</p>
+                    <IoIosArrowDropdownCircle />
+
+                    <div
+                      className="invisible absolute left-[50%]
+                                    translate-x-[-50%] translate-y-[80%]
+                                 top-[50%]
+                                flex flex-col rounded-md bg-richblack-5 p-4 text-richblack-900
+                                opacity-0 transition-all duration-200 group-hover:visible
+                                group-hover:opacity-100 lg:w-[300px]"
+                    >
+                      <div
+                        className="absolute left-[50%] top-0
+                                translate-x-[80%]
+                                translate-y-[-45%] h-6 w-6 rotate-45 rounded bg-richblack-5"
+                      ></div>
+
+                      {subLinks.length ? (
+                        subLinks.map((subLink, index) => (
+                          <Link to={`${subLink.link}`} key={index}>
+                            <p>{subLink.title}</p>
+                          </Link>
+                        ))
+                      ) : (
+                        <div></div>
+                      )}
+                    </div>
+                  </div>
+                ) : (
+                  <Link to={link?.path}>
+                    <p
+                      className={`${
+                        matchRoute(link?.path)
+                          ? "text-yellow-25"
+                          : "text-richblack-25"
+                      }`}
+                    >
+                      {link.title}
+                    </p>
+                  </Link>
+                )}
+              </li>
+            ))}
+          </ul>
         </nav>
-        <div>
-          {token !== null ? (
-            <div className="w-40 h-10 flex gap-2 items-center text-richblack-200 justify-evenly">
-              <FiSearch className="w-5 h-5 cursor-pointer" />
-              {user && user?.accountType !== "Instructor" && (
-                <Link to={"/dashboard/cart"} className="relative">
-                  <FiShoppingCart className="w-5 h-5 cursor-pointer" />
-                  {totalItems > 0 && (
-                    <span className="absolute -top-3 -right-2 text-yellow-200 font-inter text-base font-light">
-                      {totalItems}
-                    </span>
-                  )}
-                </Link>
-              )}
-              <FiUser
-                onClick={() => dispatch(logoutUser())}
-                className="w-7 h-7 rounded-full cursor-pointer"
-              />
-            </div>
-          ) : (
-            <div className="w-40 max-h-10 flex gap-5">
-              <Link
-                to={"/login"}
-                onClick={() => dispatch(loginUser())}
-                className="h-8 flex items-center justify-center w-full rounded-md px-2 bg-richblack-800 border-b border-richblack-700 text-base font-medium text-richblack-100 hover:scale-95 transition-all duration-200"
-              >
-                Log in
-              </Link>
-              <Link
-                to={"/signup"}
-                onClick={() => dispatch(loginUser())}
-                className="h-8 flex items-center justify-center w-full rounded-md  px-2 bg-richblack-800 border-b border-richblack-700 text-base font-medium text-richblack-100 hover:scale-95 transition-all duration-200"
-              >
-                Sign in
-              </Link>
-            </div>
+
+        {/* Login/SignUp/Dashboard */}
+        <div className="flex gap-x-4 items-center">
+          {user && user?.accountType != "Instructor" && (
+            <Link to="/dashboard/cart" className="relative">
+              <AiOutlineShoppingCart />
+              {totalItems > 0 && <span>{totalItems}</span>}
+            </Link>
           )}
+          {token === null && (
+            <Link to="/login">
+              <button className="border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100 rounded-md">
+                Log in
+              </button>
+            </Link>
+          )}
+          {token === null && (
+            <Link to="/signup">
+              <button className="border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100 rounded-md">
+                Sign Up
+              </button>
+            </Link>
+          )}
+          {token !== null && <ProfileDropdown />}
         </div>
       </div>
     </div>
