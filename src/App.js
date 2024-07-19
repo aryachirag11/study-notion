971c0b04a4992c1239b0e166a8aa6fdeb4f48dcf
aryachirag11 <aryachirag11@gmail.com>
2024-07-19 15:11:00 +0530

    add wrapper of OpenRoute on auth pages

--- src/App.js
+++ src/App.js
@@ -4,14 +4,38 @@ import Home from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import Navbar from "./components/common/Navbar";
+import ForgotPassword from "./pages/ForgotPassword";
+import OpenRoute from "./components/Auth/OpenRoute";
 const App = () => {
   return (
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
+        <Route
+          path="/login"
+          element={
+            <OpenRoute>
+              <LoginPage />
+            </OpenRoute>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <OpenRoute>
+              <SignupPage />
+            </OpenRoute>
+          }
+        />
+        <Route
+          path="/forgot-password"
+          element={
+            <OpenRoute>
+              <ForgotPassword />
+            </OpenRoute>
+          }
+        />
       </Routes>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
+import { Toaster } from "react-hot-toast";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
+        <Toaster />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
